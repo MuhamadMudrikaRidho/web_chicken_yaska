1febe92a3cd588084cc6ddbf656a38342e2ec6eb
mrizkibahtiar <muhammadrizkibaht@gmail.com>
2024-11-22 19:23:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import os
 from os.path import join, dirname
-from dotenv import load_dotenv
+from dotenv import load_dotenv  # type: ignore
 
-from flask import (
+from flask import (  # type: ignore
     Flask,
     request,
     render_template,
@@ -11,34 +11,38 @@ from flask import (
     jsonify
 )
 
-from pymongo import MongoClient
-from bson import ObjectId
+from pymongo import MongoClient  # type: ignore
+from bson import ObjectId  # type: ignore
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
 MONGODB_URI = os.environ.get("MONGODB_URI")
-DB_NAME =  os.environ.get("DB_NAME")
+DB_NAME = os.environ.get("DB_NAME")
 
 client = MongoClient(MONGODB_URI)
 db = client[DB_NAME]
 
 app = Flask(__name__)
 
+
 @app.route("/")
-def home() :
-  return render_template('index.html', login=False)
+def home():
+    return render_template('index.html', login=False)
+
 
 @app.route("/menu")
-def menu() :
-  return render_template('menu.html', login=False)
+def menu():
+    return render_template('menu.html', login=False)
+
 
 @app.route('/about')
-def about() :
-  return render_template('about.html', login=False)
+def about():
+    return render_template('about.html', login=False)
 
-port=5000
-debug=True
 
-if __name__ == "__main__" :
-  app.run('0.0.0.0', port=port, debug=debug)
+port = 5000
+debug = True
+
+if __name__ == "__main__":
+    app.run('0.0.0.0', port=port, debug=debug)
