d566dd1c26c0af4fba8f6ac7060eed959895f577
LIan2nd <alfiannurusyaid19@gmail.com>
2024-12-02 14:23:02 +0700

    smooth tapi kasar

--- menu-factory.py
+++ menu-factory.py
@@ -19,13 +19,13 @@ db.menu.drop()
 
 doc = [
   {"image": "assets/images/hand-picked/favyasaka.jpg",
-    "name": "Ayam Bakar Crispy", "price": 15000},
+    "name": "Ayam Bakar Crispy", "price": 15000, "category" : "Ayam Spicy", "description" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero eum non nihil exercitationem, consequuntur nobis ullam, eveniet laudantium, repudiandae voluptatem ex sunt iusto consectetur cumque?"},
   {"image": "assets/images/hand-picked/chicken.jpg",
-    "name": "Chicken + Nasi", "price": 20000},
+    "name": "Chicken + Nasi", "price": 20000, "category" : "Paket Hemat", "description" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam alias vitae eos, corrupti unde ex!"},
   {"image": "assets/images/hand-picked/mentai.jpg",
-    "name": "Ayam Bakar Mentai + Nasi", "price": 15000},
+    "name": "Ayam Bakar Mentai + Nasi", "price": 15000, "category" : "Paket Hemat", "description" : "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente aspernatur facilis a animi, excepturi maiores soluta eaque minima, ipsa totam laboriosam sit iusto fuga mollitia recusandae ullam consequuntur quia facere! Esse eaque ratione quos deleniti sunt. Ad, cumque hic. Porro."},
   {"image": "assets/images/hand-picked/geprek.jpg",
-    "name": "Ayam Geprek + Nasi", "price": 17000}
+    "name": "Ayam Geprek + Nasi", "price": 17000, "category" : "Paket Hemat", "description" : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe similique consectetur nihil aspernatur nesciunt possimus corrupti totam minus quod qui? Fugiat accusantium quasi, nostrum nam repellat quae aliquam perferendis corporis?"}
 ]
 
 db.menu.insert_many(doc)
