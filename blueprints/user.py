89f354817b2fb9911993820fe9a29ce8f2931a52
MuhamadMudrikaRidho <mudrikaridho16@gmail.com>
2024-12-11 11:36:08 +0700

    updete foto profil

--- blueprints/user.py
+++ blueprints/user.py
@@ -1,14 +1,26 @@
-from flask import Blueprint, render_template, request, redirect, url_for, session, flash, current_app
+from flask import Blueprint, render_template, request, redirect, url_for, session, flash, current_app, send_from_directory
 from datetime import datetime
 import bcrypt
 import re
+import os
+from werkzeug.utils import secure_filename
 
 user_bp = Blueprint("user", __name__, url_prefix="/account")
+
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@user_bp.route('/uploads/<path:filename>')
+def uploaded_file(filename):
+    upload_folder = current_app.config['UPLOAD_FOLDER']
+    return send_from_directory(upload_folder, filename)
 
 @user_bp.route('/', methods=["GET", "POST"])
 def home():
     db = current_app.config['DB']
-    
+
     username = session.get('username')
     if not username:
         return redirect(url_for('auth.login'))
@@ -64,7 +76,7 @@ def home():
 @user_bp.route('/edit_address', methods=["POST"])
 def edit_address():
     db = current_app.config['DB']
-    
+
     username = session.get('username')
     if not username:
         return redirect(url_for('auth.login'))
@@ -91,4 +103,38 @@ def edit_address():
     )
 
     flash("Alamat berhasil diperbarui!", "success")
+    return redirect(url_for('user.home'))
+
+@user_bp.route('/upload_profile_pic', methods=["POST"])
+def upload_profile_pic():
+    db = current_app.config['DB']
+    upload_folder = current_app.config["UPLOAD_FOLDER"]
+    profile_folder = os.path.join(upload_folder, 'profiles')
+
+    if not os.path.exists(profile_folder):
+        os.makedirs(profile_folder)
+
+    username = session.get('username')
+    if not username:
+        flash("Anda harus login untuk mengubah foto profil.", "danger")
+        return redirect(url_for('auth.login'))
+
+    image = request.files.get('profile_pic')
+    if image and allowed_file(image.filename):
+        filename = secure_filename(image.filename)
+        today = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+        file_fullname = f"profile-{username}-{today}.{filename.rsplit('.', 1)[1].lower()}"
+        file_path = os.path.join(profile_folder, file_fullname)
+        image.save(file_path)
+
+        # Update path foto profil di database
+        image_url = f'uploads/profiles/{file_fullname}'
+        db.users.update_one(
+            {"username": username},
+            {"$set": {"profile_pic": image_url}}
+        )
+        flash("Foto profil berhasil diperbarui!", "success")
+    else:
+        flash("File yang diunggah tidak valid. Pastikan format file PNG, JPG, atau JPEG.", "danger")
+
     return redirect(url_for('user.home'))
